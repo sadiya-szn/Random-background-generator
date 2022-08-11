8fa1546ebdf05e6c1938021f8826cef89f32e56f
sadiya <104234800+sadiya-szn@users.noreply.github.com>
2022-08-10 17:26:26 -0700

    commit

--- main.js
+++ main.js
@@ -1,11 +1,5 @@
-function setBg () {
-    randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    randomColorr = Math.floor(Math.random() * 16777215).toString(16);
-document.body.style.background ="linear-gradient(to right, " + "#" + randomColor + ","  + "#" + randomColorr + ")";
-// document.getElementById('color').innerHTML = "#" + randomColor;
-}
 
 
-var getBg = document.getElementById('getbg');
-getBg.addEventListener('click', setBg);
-setBg();
+
+
+
